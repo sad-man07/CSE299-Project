2bd0cb5151cba6d59e8f254bd29b9423db3193a5
Fahim Hasan Pradhan <fahim.pradhan@northsouth.edu>
2024-03-06 07:01:06 +0600

    .

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ function onSignIn(googleUser) {
     $("#name").text(profile.getName());
     $("#email").text(profile.getEmail());
     $("#image").attr('src', profile.getImageUrl());
-    $("#.data").css("display", "block");
+    $(".data").css("display", "block");
     $(".g-signin2").css("display", "none");
-  }
+    var accessToken = googleUser.getAuthResponse().access_token;
+    // Call fetchUserProfile function if it's defined
+    if (typeof fetchUserProfile === 'function') {
+        fetchUserProfile(accessToken);
+    }
+}
+
 
 function signOut(){
     var auth2 = gapi.auth2.getAuthInstance();
